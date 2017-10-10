8b490d17d714fd1b8ab19a57c87966e514aff12b
Michael <berryintlonline@gmail.com>
2017-10-10 09:13:29 -0600

    Remove composer.json configuration

--- src/PackagerGetCommand.php
+++ src/PackagerGetCommand.php
@@ -57,7 +57,7 @@ class PackagerGetCommand extends Command
     public function handle()
     {
         // Start the progress bar
-        $bar = $this->helper->barSetup($this->output->createProgressBar(5));
+        $bar = $this->helper->barSetup($this->output->createProgressBar(4));
         $bar->start();
 
         // Common variables
@@ -72,11 +72,8 @@ class PackagerGetCommand extends Command
         }
         $path = getcwd().'/packages/';
         $fullPath = $path.$vendor.'/'.$name;
-        $requirement = '"psr-4": {
-            "'.$vendor.'\\\\'.$name.'\\\\": "packages/'.$vendor.'/'.$name.'/src",';
-        $appConfigLine = 'App\Providers\RouteServiceProvider::class,
-
-        '.$vendor.'\\'.$name.'\\'.$name.'ServiceProvider::class,';
+        //$requirement = '"psr-4": {"'.$vendor.'\\\\'.$name.'\\\\": "packages/'.$vendor.'/'.$name.'/src",';
+        //$appConfigLine = 'App\Providers\RouteServiceProvider::class,'.$vendor.'\\'.$name.'\\'.$name.'ServiceProvider::class,';
 
         // Start creating the package
         $this->info('Creating package '.$vendor.'\\'.$name.'...');
@@ -102,11 +99,11 @@ class PackagerGetCommand extends Command
         $bar->advance();
 
         // Add it to composer.json
-        $this->info('Adding package to composer and app...');
-            $this->helper->replaceAndSave(getcwd().'/composer.json', '"psr-4": {', $requirement);
+        //$this->info('Adding package to composer and app...');
+        //    $this->helper->replaceAndSave(getcwd().'/composer.json', '"psr-4": {', $requirement);
             // And add it to the providers array in config/app.php
-            $this->helper->replaceAndSave(getcwd().'/config/app.php', 'App\Providers\RouteServiceProvider::class,', $appConfigLine);
-        $bar->advance();
+        //    $this->helper->replaceAndSave(getcwd().'/config/app.php', 'App\Providers\RouteServiceProvider::class,', $appConfigLine);
+        //$bar->advance();
 
         // Finished creating the package, end of the progress bar
         $bar->finish();
